ca4d32a87fe97bab2aa493c64848682f67058f45
LeonMilosevic <leonleon787@yahoo.com>
2020-06-09 22:56:42 +0300

    animations home added

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+// import third party
+import { gsap } from "gsap";
+import { Link, useHistory } from "react-router-dom";
 // import components
 import Nav from "./Nav";
 // import images
 import panImage from "../../assets/pan.svg";
 
-const Home = (props) => {
+const Home = () => {
+  const history = useHistory();
+  let homeInspireRef = useRef(null);
+  let homeImageRef = useRef(null);
+  const navRef = React.createRef(null);
+  const tl = new gsap.timeline();
+  const changeFromHomeToAny = (destination) => (e) => {
+    e.preventDefault();
+    tl.reverse();
+    setTimeout(() => {
+      history.push(destination);
+    }, 1600);
+  };
+  useEffect(() => {
+    tl.from(navRef.current, { yPercent: -100, duration: 1 });
+    tl.to(homeInspireRef, { opacity: 1, duration: 0.5 });
+    tl.to(homeImageRef, { opacity: 1, duration: 0.5 }, 1);
+  }, [tl, navRef]);
   return (
     <div className="container-fluid">
-      <Nav />
-      <div className="home-inspire">
+      <Nav
+        ref={navRef}
+        changeFromAnyPageToRegister={changeFromHomeToAny("/register")}
+        changeFromAnyPageToLogin={changeFromHomeToAny("/login")}
+      />
+      <div ref={(el) => (homeInspireRef = el)} className="home-inspire">
         <div className="home-inspire_header">Create, Track</div>
         <div className="home-inspire_header">Cook!</div>
         <div className="home-inspire_info">
@@ -25,7 +48,7 @@ const Home = (props) => {
           </Link>
         </div>
       </div>
-      <div className="home-image_container">
+      <div ref={(el) => (homeImageRef = el)} className="home-image_container">
         <img className="home-image" src={panImage} alt="pan_img" />
       </div>
     </div>
--- src/components/main/Nav.js
+++ src/components/main/Nav.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Nav = () => {
+const Nav = React.forwardRef((props, navRef) => {
   return (
-    <nav className="my-nav">
+    <nav ref={navRef} className="my-nav">
       <div>
         <NavLink className="empty-link nav-link" to="/">
           Home
@@ -13,6 +13,7 @@ const Nav = () => {
         <NavLink
           activeClassName="active-link"
           className="empty-link nav-link"
+          onClick={props.changeFromAnyPageToRegister}
           to="/register"
         >
           Get started
@@ -20,6 +21,7 @@ const Nav = () => {
         <NavLink
           activeClassName="active-link"
           className="empty-link nav-link"
+          onClick={props.changeFromAnyPageToLogin}
           to="/login"
         >
           Log in
@@ -27,6 +29,6 @@ const Nav = () => {
       </div>
     </nav>
   );
-};
+});
 
 export default Nav;
